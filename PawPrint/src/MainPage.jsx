e2f863fbb63a93fda613e2d56184eee3e7e02114
Gabriel <117957176+g0DTuRtI3@users.noreply.github.com>
2025-02-02 10:50:46 -0500

    final

--- PawPrint/src/MainPage.jsx
+++ PawPrint/src/MainPage.jsx
@@ -1,16 +1,53 @@
+import React, { useState } from "react";
 import MainPageTables from './MainPageTables';
 import './MainPage.css';
 import MainPageImages from './MainPageImages';
 import MainPageButtons from './MainPageButtons';
+import EditForm from './EditForm';
 
 function MainPage() {
+    const [showForm, setShowForm] = useState(false);
+    const [formData, setFormData] = useState({
+        petName: '',
+        breed: '',
+        gender: '',
+        id: '',
+        status: '',
+        age: '',
+        weight: '',
+        allergies: '',
+        vaccination: '',
+        checkUpDate: '',
+        medication: '',
+        ownerName: '',
+        phoneNumber: '',
+        ownerGender: '',
+        email: '',
+        address: ''
+    });
+
+    const openForm = () => {
+        setShowForm(true);
+    };
+
+    const closeForm = () => {
+        setShowForm(false);
+    };
+
+    const handleSave = (data) => {
+        setFormData(data);  // Update the form data in MainPage
+    };
+
     return (
         <div className="wrapper">
-                <div className="rectangleBorder">
-                    <MainPageTables></MainPageTables>
-                    <MainPageImages></MainPageImages>
-                    <MainPageButtons></MainPageButtons>
-                </div>
+            <div className="rectangleBorder">
+                <MainPageTables formData={formData} />
+                <MainPageImages />
+                <MainPageButtons onEditClick={openForm} />
+
+                {/* Conditionally render the EditForm */}
+                {showForm && <EditForm onClose={closeForm} onSave={handleSave} />}
+            </div>
         </div>
     );
 }
--- PawPrint/src/MainPageTables.jsx
+++ PawPrint/src/MainPageTables.jsx
@@ -1,27 +1,27 @@
 import './MainPage.css';
 
-function MainPageTables() {
+function MainPageTables({ formData }) {
     return (
         <div className="tables-container">
             <div className="generalTable">
                 <div className="table-row">
-                    <span className="label">Name</span>
+                    <span className="label">{formData.petName}</span>
                     <div className="table-cell"></div>
                 </div>
                 <div className="table-row">
-                    <span className="label">Breed</span>
+                    <span className="label" id="breedValue">{formData.breed}</span>
                     <div className="table-cell"></div>
                 </div>
                 <div className="table-row">
-                    <span className="label">Gender</span>
+                    <span className="label" id="genderValue">{formData.gender}</span>
                     <div className="table-cell"></div>
                 </div>
                 <div className="table-row">
-                    <span className="label">ID</span>
+                    <span className="label" id="id_Value">{formData.id}</span>
                     <div className="table-cell"></div>
                 </div>
                 <div className="table-row">
-                    <span className="label">Status</span>
+                    <span className="label" id="statusValue">{formData.status}</span>
                     <div className="table-cell"></div>
                 </div>
             </div>
@@ -31,59 +31,59 @@ function MainPageTables() {
                 <div className='medicalTable'>
                     <div className="table-row2">
                         <span className="label2">Age:</span>
-                        <span className="label2" id="ageValue"></span>
+                        <span className="label2" id="ageValue">{formData.age}</span>
                         <div className="table-cell2"></div>
                     </div>
                     <div className="table-row2">
                         <span className="label2">Weight:</span>
-                        <span className="label2" id="weightValue"></span>
+                        <span className="label2" id="weightValue">{formData.weigth}</span>
                         <div className="table-cell2"></div>
                     </div>
                     <div className="table-row2">
                         <span className="label2">Allergies:</span>
-                        <span className="label2" id="allergiesValue"></span>
+                        <span className="label2" id="allergiesValue">{formData.allergies}</span>
                         <div className="table-cell2"></div>
                     </div>
                     <div className="table-row2">
                         <span className="label2">Vaccination:</span>
-                        <span className="label2" id="vaccinationValue"></span>
+                        <span className="label2" id="vaccinationValue">{formData.vaccination}</span>
                         <div className="table-cell2"></div>
                     </div>
                     <div className="table-row2">
                         <span className="label2">Check Up Date:</span>
-                        <span className="label2" id="checkUpDateValue"></span>
+                        <span className="label2" id="checkUpDateValue">{formData.checkUpDate}</span>
                         <div className="table-cell2"></div>
                     </div>
                     <div className="table-row2">
                         <span className="label2">Medication:</span>
-                        <span className="label2" id="medicationValue"></span>
+                        <span className="label2" id="medicationValue">{formData.medication}</span>
                         <div className="table-cell2"></div>
                     </div>
                 </div>
                 <div className='ownerTable'>
                     <div className="table-row2">
                         <span className="label2">Name:</span>
-                        <span className="label2" id="nameValue"></span>
+                        <span className="label2" id="nameValue">{formData.ownerName}</span>
                         <div className="table-cell2"></div>
                     </div>
                     <div className="table-row2">
                         <span className="label2">Phone Number:</span>
-                        <span className="label2" id="phoneNumberValue"></span>
+                        <span className="label2" id="phoneNumberValue">{formData.phoneNumber}</span>
                         <div className="table-cell2"></div>
                     </div>
                     <div className="table-row2">
-                        <span className="label2">Pronouns:</span>
-                        <span className="label2" id="pronounsValue"></span>
+                        <span className="label2">Gender:</span>
+                        <span className="label2" id="ownerGenderValue">{formData.ownerGender}</span>
                         <div className="table-cell2"></div>
                     </div>
                     <div className="table-row2">
                         <span className="label2">Email:</span>
-                        <span className="label2" id="emailValue"></span>
+                        <span className="label2" id="emailValue">{formData.email}</span>
                         <div className="table-cell2"></div>
                     </div>
                     <div className="table-row2">
                         <span className="label2">Address:</span>
-                        <span className="label2" id="addressValue"></span>
+                        <span className="label2" id="addressValue">{formData.address}</span>
                         <div className="table-cell2"></div>
                     </div>
                 </div>
